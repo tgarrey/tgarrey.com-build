890701d78d89f9de7d95214334afe315e99d2587
Tomás García <tgarrey@gmail.com>
2018-11-16 20:33:52 +0000

    First commit

---
+++ precache-manifest.125257800a6bc363a832bc3c1f5db184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "849d68cf91beddcc8e0d",
+    "url": "/static/js/main.849d68cf.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "849d68cf91beddcc8e0d",
+    "url": "/static/css/main.3674082e.chunk.css"
+  },
+  {
+    "revision": "d8c9771b09b55c162fd2549ff58e9a17",
+    "url": "/index.html"
+  }
+];
